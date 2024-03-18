4a8dddc1e37c3334c98d86c3cbc9ab2b750381dd
Alexsandr <larinalexsandr9091@gmail.com>
2024-03-18 21:51:02 +0200

    add cors

--- index.js
+++ index.js
@@ -4,9 +4,11 @@ const PORT = process.env.PORT || 5000
 const MONGO_URL = process.env.MONGO_URL
 const mongoose = require('mongoose')
 const apiRouter = require('./routers/apiRouter')
+const cors = require('cors')
 
 const app = express()
 
+app.use(cors())
 app.use(express.json())
 app.use('/api', apiRouter)
 
